4d88c3dc0326d147f18da03c2501eaa66f7d7c7d
Matthew Koontz <mjkoontz93@gmail.com>
2025-05-08 22:07:40 -0700

    fastmath

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,29 @@ use std::io::BufReader;
 use std::env;
 use std::collections::{HashMap, BinaryHeap};
 use std::cmp::Reverse;
+use fast_fp::{FF32};
 
-type V3 = (f64, f64, f64);
+type Float = FF32;
+type V3 = (Float, Float, Float);
 
 #[derive(Debug)]
 struct StarSystem {
     pub name: String,
     pub main_star_type: String,
     pub coords: V3,
-    pub distance_from_sol: f64,
+    pub distance_from_sol: Float,
     pub is_neutron: bool,
 }
 
-fn square(a: f64) -> f64 {
+fn square(a: Float) -> Float {
     a * a
 }
 
-fn distance_v(a: &V3, b: &V3) -> f64 {
+fn distance_v(a: &V3, b: &V3) -> Float {
     (square(a.0 - b.0) + square(a.1 - b.1) + square(a.2 - b.2)).sqrt()
 }
 
-fn distance(a: &StarSystem, b: &StarSystem) -> f64 {
+fn distance(a: &StarSystem, b: &StarSystem) -> Float {
     distance_v(&a.coords, &b.coords)
 }
 
@@ -51,11 +53,11 @@ impl Eq for StarSystem {}
 #[derive(Debug, serde::Deserialize)]
 struct StarSystemRecord {
     pub name: String,
-    pub Coord_X: f64,
-    pub Coord_Y: f64,
-    pub Coord_Z: f64,
+    pub Coord_X: f32,
+    pub Coord_Y: f32,
+    pub Coord_Z: f32,
     pub mainStarType: String,
-    pub d_from_sol: f64,
+    pub d_from_sol: f32,
 }
 
 impl From<StarSystemRecord> for StarSystem {
@@ -70,9 +72,9 @@ impl From<StarSystemRecord> for StarSystem {
         } = record;
         StarSystem {
             name,
-            coords: (Coord_X, Coord_Y, Coord_Z),
+            coords: (Coord_X.into(), Coord_Y.into(), Coord_Z.into()),
             is_neutron: mainStarType == "Neutron Star",
-            distance_from_sol: d_from_sol,
+            distance_from_sol: d_from_sol.into(),
             main_star_type: mainStarType,
         }
     }
@@ -95,10 +97,10 @@ fn read_star_systems(filename: &str, filter: fn(&StarSystem) -> bool) -> Vec<Sta
     return retval;
 }
 
-fn neighbors(systems: &Vec<StarSystem>, system_idx: usize, mut jump_distance: f64) -> Vec<usize> {
+fn neighbors(systems: &Vec<StarSystem>, system_idx: usize, mut jump_distance: Float) -> Vec<usize> {
     let system = &systems[system_idx];
     if system.is_neutron {
-        jump_distance *= 4.0;
+        jump_distance *= Float::from(4.0);
     }
     let mut retval = Vec::new();
     retval.reserve(256);
@@ -128,7 +130,7 @@ fn neighbors(systems: &Vec<StarSystem>, system_idx: usize, mut jump_distance: f6
 #[derive(Debug,Copy,Clone)]
 struct HScore {
     pub jumps: i64,
-    pub distance: f64,
+    pub distance: Float,
 }
 
 impl Ord for HScore {
@@ -152,26 +154,26 @@ impl PartialEq for HScore {
 impl Eq for HScore {}
 
 
-fn a_star(systems: &Vec<StarSystem>, start_idx: usize, goal_idx: usize, jump_distance: f64) -> Option<Vec<usize>> {
+fn a_star(systems: &Vec<StarSystem>, start_idx: usize, goal_idx: usize, jump_distance: Float) -> Option<Vec<usize>> {
     let start = &systems[start_idx];
     let goal = &systems[goal_idx];
     let h_fn = |system_idx: usize| -> HScore {
         let system = &systems[system_idx];
         if system_idx == goal_idx {
             let jumps = 0;
-            let distance = 0.0;
+            let distance: Float = 0.0.into();
             HScore{jumps, distance}
         } else if systems[system_idx].is_neutron {
             let distance = distance(system, goal);
-            let after_neutron_distance = distance - (jump_distance * 4.0);
-            if after_neutron_distance <= 0.0 {
+            let after_neutron_distance: Float = distance - (jump_distance * Float::from(4.0));
+            if after_neutron_distance <= 0.0.into() {
                 return HScore{jumps: 1, distance};
             }
-            let jumps = (after_neutron_distance / jump_distance).ceil() as i64 + 1;
+            let jumps = f32::from((after_neutron_distance / jump_distance).ceil()) as i64 + 1;
             HScore{jumps, distance}
         } else {
             let distance = distance(system, goal);
-            let jumps = (distance / jump_distance).ceil() as i64;
+            let jumps = f32::from((distance / jump_distance).ceil()) as i64;
             HScore{jumps, distance}
         }
     };
@@ -180,7 +182,7 @@ fn a_star(systems: &Vec<StarSystem>, start_idx: usize, goal_idx: usize, jump_dis
     let mut h_score = HashMap::new();
     let mut to_visit = BinaryHeap::new();
     
-    let start_h_score = HScore{jumps:0, distance:0.0};
+    let start_h_score = HScore{jumps:0, distance:0.0.into()};
     g_score.insert(start_idx, start_h_score);
     h_score.insert(start_idx, start_h_score);
     to_visit.push(Reverse((start_h_score, start_idx)));
@@ -235,7 +237,7 @@ fn a_star(systems: &Vec<StarSystem>, start_idx: usize, goal_idx: usize, jump_dis
     None
 }
 
-fn neutron_a_star(systems: &Vec<StarSystem>, start_idx: usize, goal_idx: usize, jump_distance: f64) -> Option<Vec<usize>> {
+fn neutron_a_star(systems: &Vec<StarSystem>, start_idx: usize, goal_idx: usize, jump_distance: Float) -> Option<Vec<usize>> {
     let start = &systems[start_idx];
     let goal = &systems[goal_idx];
 
@@ -249,51 +251,70 @@ fn neutron_a_star(systems: &Vec<StarSystem>, start_idx: usize, goal_idx: usize, 
         }
     }).collect();
 
-    let h_fn = |from_idx: usize, system_idx: usize| -> HScore {
+    println!("Neutron star count: {}", neutron_systems.len());
+
+    let neutron_distance_to_goal: Vec<Float> = neutron_systems.iter().map(|&n| {
+        let system = &systems[n];
+        distance(system, goal) / 4.0
+    }).collect();
+
+    // let n_to_n_distance: HashMap<(usize, usize), Float> = neutron_systems.iter().enumerate().flat_map(|(i, &start)| {
+        // (&neutron_systems[i+1..]).iter().map(move |&end| {
+            // ((start, end), distance(&systems[start], &systems[end]))
+        // })
+    // }).collect();
+
+    let h_fn = |from_idx: usize, system_n_idx: usize| -> HScore {
+        let system_idx = neutron_systems[system_n_idx];
         let system = &systems[system_idx];
         let from = &systems[from_idx];
         let from_distance = distance(from, system);
         let from_jumps = if from.is_neutron {
-            let after_first_jump_distance = from_distance - jump_distance * 4.0;
-            if after_first_jump_distance < 0.0 {
+            let after_first_jump_distance: Float = from_distance - jump_distance * Float::from(4.0);
+            if after_first_jump_distance < 0.0.into() {
                 1
             } else {
-                (after_first_jump_distance / (jump_distance)).ceil() as i64 + 1
+                f32::from((after_first_jump_distance / (jump_distance)).ceil()) as i64 + 1
             }
         } else {
-            (from_distance / jump_distance).ceil() as i64
+            f32::from((from_distance / jump_distance).ceil()) as i64
         };
-        let goal_distance = distance(system, goal);
-        let goal_jumps = (goal_distance / (jump_distance * 4.0)).ceil() as i64;
+        let goal_distance = neutron_distance_to_goal[system_n_idx];
+        let goal_jumps = f32::from((goal_distance / jump_distance).ceil()) as i64;
         let jumps = from_jumps + goal_jumps;
-        let distance = from_distance + goal_distance;
+        let distance = from_distance + (goal_distance * Float::from(4.0));
         HScore{jumps, distance}
     };
 
 
     let mut g_score = HashMap::new();
+    g_score.reserve(neutron_systems.len() * 2);
     let mut h_score = HashMap::new();
+    h_score.reserve(neutron_systems.len() * 2);
     let mut parent: HashMap<usize, usize> = HashMap::new();
+    parent.reserve(neutron_systems.len() * 2);
     let mut to_visit = BinaryHeap::new();
-    to_visit.reserve(neutron_systems.len()+1);
+    to_visit.reserve(neutron_systems.len() * 2);
 
-    let start_h_score = HScore{jumps: 0, distance: 0.0};
+    let start_h_score = HScore{jumps: 0, distance: 0.0.into()};
     g_score.insert(start_idx, start_h_score);
     h_score.insert(start_idx, start_h_score);
     
     let no_neutron_path = a_star(systems, start_idx, goal_idx, jump_distance)?;
     let no_neutron_len = no_neutron_path.len() as i64;
     println!("No neutron length: {}", no_neutron_len);
-    let no_neutron_h_score = HScore{jumps: no_neutron_len, distance: 0.0};
+    let no_neutron_h_score = HScore{jumps: no_neutron_len, distance: 0.0.into()};
     h_score.insert(goal_idx, no_neutron_h_score);
     parent.insert(goal_idx, start_idx);
 
     to_visit.push(Reverse((no_neutron_h_score, goal_idx)));
-    for &n_idx in &neutron_systems {
-        let h = h_fn(start_idx, n_idx);
-        *h_score.entry(n_idx).or_insert(h) = h;
-        to_visit.push(Reverse((h, n_idx)));
-        *parent.entry(n_idx).or_insert(start_idx) = start_idx;
+    for (n_idx_idx, &n_idx) in neutron_systems.iter().enumerate() {
+        let h = h_fn(start_idx, n_idx_idx);
+        if h < no_neutron_h_score {
+            *h_score.entry(n_idx).or_insert(h) = h;
+            to_visit.push(Reverse((h, n_idx)));
+            *parent.entry(n_idx).or_insert(start_idx) = start_idx;
+        }
     }
 
     while let Some(Reverse((current_h_score, current_idx))) = to_visit.pop() {
@@ -323,10 +344,11 @@ fn neutron_a_star(systems: &Vec<StarSystem>, start_idx: usize, goal_idx: usize, 
         let from_path_distance = distance(parent_s, current);
         let parent_g_score = g_score[&parent_idx];
         let cur_g_score = HScore{jumps: parent_g_score.jumps + from_path_len, distance:parent_g_score.distance + from_path_distance};
+        println!("[N] Queue length: {}", to_visit.len());
         println!("[N] Processing {} {:?}\n {:?} {:?}", systems[current_idx].name, systems[current_idx].coords, cur_g_score, current_h_score);
         *g_score.entry(current_idx).or_insert(cur_g_score) = cur_g_score;
         let to_goal_distance = distance(current, goal);
-        let to_goal_jumps = (to_goal_distance / jump_distance).ceil() as i64;
+        let to_goal_jumps = f32::from((to_goal_distance / jump_distance).ceil()) as i64;
         let to_goal_h_score = HScore {
             distance: cur_g_score.distance + to_goal_distance,
             jumps: cur_g_score.jumps + to_goal_jumps,
@@ -337,12 +359,15 @@ fn neutron_a_star(systems: &Vec<StarSystem>, start_idx: usize, goal_idx: usize, 
             *parent.get_mut(&goal_idx).unwrap() = current_idx;
         }
 
+        let d_from_sol_cutoff = if to_goal_distance < total_distance {to_goal_distance} else {total_distance};
 
-        for &neighbor_idx in &neutron_systems {
-            if neighbor_idx == current_idx {
+        for (n_idx_idx, &neighbor_idx) in neutron_systems.iter().enumerate() {
+            let neighbor = &systems[neighbor_idx];
+            let d_from_sol_diff = (neighbor.distance_from_sol - current.distance_from_sol).abs();
+            if d_from_sol_diff >= d_from_sol_cutoff - neutron_distance_to_goal[n_idx_idx] {
                 continue;
             }
-            let mut new_h_score = h_fn(current_idx, neighbor_idx);
+            let mut new_h_score = h_fn(current_idx, n_idx_idx);
             new_h_score.jumps += cur_g_score.jumps;
             new_h_score.distance += cur_g_score.distance;
             if new_h_score >= no_neutron_h_score {
@@ -396,7 +421,7 @@ fn main() {
         
     //};
 
-    let path = neutron_a_star(&systems, start_idx, goal_idx, 61.0).unwrap();
+    let path = neutron_a_star(&systems, start_idx, goal_idx, 61.0.into()).unwrap();
     let path_len = path.len();
     for system_idx in path {
         let system = &systems[system_idx];
